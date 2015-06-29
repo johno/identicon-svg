6c58536009481c35a25b64a17946e5f39572afaf
John Otander <johnotander@gmail.com>
2015-06-29 17:01:23 -0600

    Add support for symmetry and complementary colors

--- Identicon.js
+++ Identicon.js
@@ -3,10 +3,13 @@ var chroma = require('chroma-js')
 var sample = require('lodash').sample
 var randomHexColor = require('random-hex-color')
 
-var Identicon = React.createClass({displayName: "Identicon",
+var Identicon = React.createClass({
+  displayName: 'Identicon',
   getDefaultProps: function() {
     return {
-      size: 120
+      size: 120,
+      colors: [],
+      symmetrical: true
     }
   },
 
@@ -16,14 +19,18 @@ var Identicon = React.createClass({displayName: "Identicon",
     var viewBox = [0, 0, size, size].join(' ')
     var colorBoxSize = size / 6
 
-    var baseColors = [randomHexColor(), randomHexColor()]
+    var randomColor = this.props.colors.length ? sample(colors) : randomHexColor()
+    var baseColors = [randomColor, getComplementaryColor(randomColor)]
     var scale = chroma.scale(baseColors).domain([0, 100, 200, 300, 400])
-    var colors = baseColors.concat([scale(100).hex(), scale(200).hex(), scale(300).hex()])
+    var identiconColors = baseColors.concat([scale(100).hex(), scale(200).hex(), scale(300).hex(), scale(400).hex()])
 
     var paths = []
-    for(var i = 0; i <= this.props.size; i += colorBoxSize) {
+    for(var i = 0; i <= this.props.size / 2; i += colorBoxSize) {
       for(var j = 0; j <= this.props.size; j += colorBoxSize) {
-        paths.push(React.createElement('path', { d: getBoxPath(i, j, colorBoxSize), fill: sample(colors), key: '' + i + j}))
+        var color = sample(identiconColors)
+        var mirrorColor = this.props.symmetrical ? color : sample(identiconColors)
+        paths.push(React.createElement('path', { d: getBoxPath(i, j, colorBoxSize), fill: color, key: '' + i + j }))
+        paths.push(React.createElement('path', { d: getBoxPath(this.props.size - i - colorBoxSize, j, colorBoxSize), fill: color, key: '' + i + j + 2 }))
       }
     }
 
@@ -50,4 +57,10 @@ function getBoxPath(x, y, z) {
     'L', x + z, y,
     'L', x, y
   ].join(' ')
+}
+
+function getComplementaryColor(color) {
+  var colorAsHex = color.replace('#', '0x') ^ '0xfffff'
+  var complement =  ('000000' + colorAsHex.toString(16)).slice(-6)
+  return '#' + complement
 }
